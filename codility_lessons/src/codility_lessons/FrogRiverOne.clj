7953a284977a3bb6a834da4f7b17a46c9d3d3335
Vinoth Kumar Asaithambi <me@geekyvin.com>
2017-07-28 18:25:16 +0100

    added-frog-river-one-problem

--- codility_lessons/src/codility_lessons/FrogRiverOne.clj
+++ codility_lessons/src/codility_lessons/FrogRiverOne.clj
@@ -1,11 +1,12 @@
 (use 'clojure.data)
 
 (defn frog-river-one [A X]
-  (let [A' [range 1 (inc X)]
+  (let [A' (range 0 (inc X))
         B []]
-   (for [i (count A)]
-     (conj B (nth A i)
-      (if (= A B) i)))))
+   (for [i A'
+         :let [B (conj B (nth A i))]
+         :when (= A' (sort B))]
+        i)))
 
 (nth [0 1 3] 2)
 
@@ -18,13 +19,31 @@
 
 (diff [1 2 3] [5 9 3 2 3 7])
 
-
 (frog-river-one [3 1 4 2 3 5 4] 5)
 
-  A[1] = 3
-  A[2] = 1
-  A[3] = 4
-  A[4] = 2
-  A[5] = 3
-  A[6] = 5
-  A[7] = 4
+(defn test_for [A]
+  (let [B nil]
+    (for [i A
+          :let [B (conj B i)]
+          :when (even? i)]
+      B)))
+
+(test_for [1 2 3 4])
+
+(defn test_loop [A]
+  (let [B []]
+    (loop [x (count A)]
+      (let [B (conj B x)]))))
+
+(test_loop [1 2 3 4])
+
+(loop [x 10]
+  (when (> x 1)
+    (println x)
+    (recur (- x 2))))
+
+
+(defn for_loop [A x]
+  (let [path (int-array x)]
+    (for [i A]
+        (into))))
--- codility_lessons/src/codility_lessons/FrogRiverOne.md
+++ codility_lessons/src/codility_lessons/FrogRiverOne.md
@@ -1,4 +1,5 @@
-A small frog wants to get to the other side of a river. The frog is initially located on one bank of the river (position 0) and wants to get to the opposite bank (position X+1). Leaves fall from a tree onto the surface of the river.
+A small frog wants to get to the other side of a river. The frog is initially located on one bank of the river (position 0) and wants to get to the opposite bank (position X+1). Leaves
+fall from a tree onto the surface of the river.
 
 You are given a zero-indexed array A consisting of N integers representing the falling leaves. A[K] represents the position where one leaf falls at time K, measured in seconds.
 
